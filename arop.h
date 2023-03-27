d29d752a22a6fdbab40f224b074fc0dcd2ade0cf
Tamal1912 <94748300+Tamal1912@users.noreply.github.com>
2023-03-27 18:49:29 +0530

    Update arop.h

--- arop.h
+++ arop.h
@@ -93,3 +93,17 @@ int insertion(int ar[], int size){
         ar[j+1]=temp;
     }
 }
+
+int selection(int arr[],int size){
+ for(int i=0;i<size-1;i++){
+    int min=i;
+    for(int j=i+1;j<size;j++){
+        if(arr[min]>arr[j]){
+            min=j;
+        }
+    }
+    int temp=arr[min];
+    arr[min]=arr[i];
+    arr[i]=temp;
+ }
+}
