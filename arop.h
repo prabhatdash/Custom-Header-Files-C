ce4c447d9e0c85a174c8afde3716c55bf4c70d79
Ayan pal <127892351+Ayan0007876@users.noreply.github.com>
2024-03-05 00:46:11 +0530

    Implemented Merge Sort

--- arop.h
+++ arop.h
@@ -145,3 +145,31 @@ void shell(int ar[], int size){
         }
     }
 }
+
+void merge(int arr[], int start, int mid, int stop) {
+    int i, j, k;
+    int n1 = mid-start+1;
+    int n2 = stop-mid;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++) L[i] = arr[start+i];
+    for (j = 0; j < n2; j++) R[j] = arr[mid+1+j];
+
+    i = j = 0;
+    k = start;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k] = L[i++];
+        else arr[k] = R[j++];
+        k++;
+    } while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int start, int stop) {
+    if (start < stop) {
+        int mid = start+(stop-start)/2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid+1, stop);
+        merge(arr, start, mid, stop);
+    }
+}
